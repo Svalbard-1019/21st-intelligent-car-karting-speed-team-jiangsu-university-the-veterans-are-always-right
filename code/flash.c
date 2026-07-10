2ac6150ad142022d6055d59cf118167708b28d0e
heliyi-love-stm32 <1948224068@qq.com>
2026-07-10 15:59:36 +0800

    Revert "Reapply "Limit autonomous tracking speed for curve testing""
    This reverts commit 6a946e20fdb51b5515039502a33d41fc33264fca.

--- code/flash.c
+++ code/flash.c
@@ -34,7 +34,7 @@
 #include "zf_common_headfile.h"
 
 float speed_pid[6]={0.5f, 1.0f, 0.0f, 0.2f, 0.4f, 3.0f};
-int16 control[5] = {6, -10, 2, 0, 0};
+int16 control[5] = {10, -10, 2, 0, 0};
 float kp;
 float ki;
 float kd;
@@ -51,7 +51,6 @@ float kd;
 #define FLASH_PREVIEW_STEPS_DEFAULT      (2)
 #define FLASH_PREVIEW_STEPS_MIN          (1)
 #define FLASH_PREVIEW_STEPS_MAX          (20)
-#define FLASH_BASE_SPEED_TEST_MAX         (6)
 
 static int16 flash_clamp_route_length(int16 length)
 {
@@ -120,8 +119,6 @@ void Flash_Read_pid(void)
         {
             control[2] = FLASH_PREVIEW_STEPS_DEFAULT;
         }
-        if(control[0] < 0) control[0] = 0;
-        if(control[0] > FLASH_BASE_SPEED_TEST_MAX) control[0] = FLASH_BASE_SPEED_TEST_MAX;
         base_speed = (float)control[0];
         daoche_speed = (float)control[1];
         preview_spets = control[2];
@@ -147,8 +144,6 @@ void Flash_Write_pid(void)
     {
         control[2] = FLASH_PREVIEW_STEPS_DEFAULT;
     }
-    if(control[0] < 0) control[0] = 0;
-    if(control[0] > FLASH_BASE_SPEED_TEST_MAX) control[0] = FLASH_BASE_SPEED_TEST_MAX;
 
     MoterPID_L.Kp = speed_pid[0];
     MoterPID_R.Kp = speed_pid[0];
